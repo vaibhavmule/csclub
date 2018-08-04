04ce69c131d611835c2c49bafc6ce0bcf64bf917
Vaibhav Mule <vaibhavmule135@gmail.com>
2018-08-04 17:00:17 +0530

    change url for better migrations

--- csclub/urls.py
+++ csclub/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('page.urls')),
-    path('jobs/', include('jobboard.urls')),
+    path('job/', include('jobboard.urls')),
 ]
