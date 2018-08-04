322c17c30ba1e73368d1e4bf352e75411e1ae440
Vaibhav Mule <vaibhavmule135@gmail.com>
2018-08-04 16:16:37 +0530

    register AddJobLog model

--- jobboard/admin.py
+++ jobboard/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from jobboard.models import Employer, EmploymentType, Job
+from jobboard.models import Employer, EmploymentType, Job, AddJobLog
 
 
 admin.site.register(Employer)
 admin.site.register(EmploymentType)
 admin.site.register(Job)
+admin.site.register(AddJobLog)
