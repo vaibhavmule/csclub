8d502ab948fd33396c7675b9722b1412bd7bfe9e
Vaibhav Mule <vaibhavmule135@gmail.com>
2018-08-08 14:59:42 +0530

    add Login and Sign up flow

--- csclub/urls.py
+++ csclub/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import include, path
 
 from blog.views import post_detail, post_by_user
+from page.views import signup
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/singup/', signup, name='signup'),
     path('', include('page.urls')),
     path('job/', include('jobboard.urls')),
     path('blog/', include('blog.urls')),
