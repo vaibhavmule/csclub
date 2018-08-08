812faa716921414438bedcb0df944f5839129a49
Vaibhav Mule <vaibhavmule135@gmail.com>
2018-08-08 12:08:05 +0530

    add login_required to edit and add new articles

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 from blog.models import Post
 from blog.forms import PostForm
@@ -22,6 +23,7 @@ def post_by_user(request, username):
     return render(request, 'blog_posts_by_user.html', {'posts': posts})
 
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -37,6 +39,7 @@ def post_edit(request, pk):
     return render(request, 'post_edit.html', {'form': form})
 
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
