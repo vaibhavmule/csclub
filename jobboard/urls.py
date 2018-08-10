482d4c3f5eb18a66cacfbd8962e4ab054b26b348
Vaibhav Mule <vaibhavmule135@gmail.com>
2018-08-10 11:47:45 +0530

    add feeds for jobboard

--- jobboard/urls.py
+++ jobboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from jobboard.views import jobs, job_detail
+from jobboard.views import jobs, job_detail, LatestJobsFeed
 
 urlpatterns = [
     path('', jobs, name='jobs'),
+    path('feed/', LatestJobsFeed(), name='job_feed'),
     path('<slug:slug>/', job_detail, name='job_detail')
 ]
--- jobboard/views.py
+++ jobboard/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.syndication.views import Feed
+from django.urls import reverse
 
 from jobboard.models import Job, Employer
 
@@ -22,3 +24,21 @@ def jobs_by_employer(request, slug):
 def companies(request):
     companies = Employer.objects.all()
     return render(request, 'companies.html', {'companies': companies})
+
+
+class LatestJobsFeed(Feed):
+    title = "CSClub JobBoard"
+    link = "/job/"
+    description = "CS Trainee Vacancy and CS Jobs"
+
+    def items(self):
+        return Job.objects.all().order_by('-date_posted')[:5]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.description
+
+    def item_link(self, item):
+        return reverse('job_detail', args=[item.slug])
