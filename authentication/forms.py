359c04551dbc4645dd938da90f5d800e14b1836d
Vaibhav Mule <vaibhavmule135@gmail.com>
2018-08-23 17:42:32 +0530

    show Publish your article and Post a job buttons and remove label from form

--- authentication/forms.py
+++ authentication/forms.py
@@ -8,6 +8,14 @@ class UserCreationForm(UserCreationForm):
         model = User
         fields = ('username', 'email')
 
+    def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'email', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+            self.fields[fieldname].widget.attrs['placeholder'] = self.fields[fieldname].label 
+            self.fields[fieldname].label = ''
+
 
 class UserChangeForm(UserChangeForm):
 
