fee0f0b64a6f5019a205ba1bdf7f046d66a3cb9c
Vaibhav Mule <vaibhavmule135@gmail.com>
2018-09-24 23:44:27 +0530

    add job controller

---
+++ app/http/controllers/JobController.py
@@ -0,0 +1,7 @@
+''' A Module Description '''
+
+class JobController:
+    ''' Class Docstring Description '''
+
+    def index(self):
+        return view('job')
--- routes/web.py
+++ routes/web.py
@@ -3,4 +3,7 @@ from masonite.routes import Get, Post
 
 ROUTES = [
     Get().route('/', 'PageController@index'),
+
+    # Jobs
+    Get().route('/job', 'JobController@index'),
 ]
