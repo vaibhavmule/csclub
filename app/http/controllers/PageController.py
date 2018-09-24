5fbd7589bdbc9895a58dda0ac43e0535d6f2a038
Vaibhav Mule <vaibhavmule135@gmail.com>
2018-09-24 23:30:00 +0530

    add PageController and bulma

---
+++ app/http/controllers/PageController.py
@@ -0,0 +1,7 @@
+''' A Module Description '''
+
+class PageController:
+    ''' Class Docstring Description '''
+
+    def index(self, Application):
+        return view('index', {'app': Application})
