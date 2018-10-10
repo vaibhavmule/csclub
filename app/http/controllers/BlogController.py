7894b14b55f9a589da163ef2e90a34d953498891
Vaibhav Mule <vaibhavmule135@gmail.com>
2018-10-10 14:09:05 +0530

    show posts by user

--- app/http/controllers/BlogController.py
+++ app/http/controllers/BlogController.py
@@ -5,6 +5,7 @@ from slugify import slugify
 from masonite.view import View
 from masonite.request import Request
 from app.Post import Post
+from app.User import User
 
 
 class BlogController:
@@ -36,6 +37,17 @@ class BlogController:
         return request.redirect('/blog')
 
     def show(self, view: View, request: Request):
-        posts = Post.where(
-            'slug', request.param('slug')).get()
-        return view.render('blog/show', {"post": posts[0]})
+        post = Post.where(
+            'slug', request.param('slug')).first_or_fail()
+        return view.render('blog/show', {"post": post})
+
+    def user_posts(self, view: View, request: Request):
+        username = request.param('username')
+        author = User.where('username', username).first_or_fail()
+        if request.user().username == username:
+            posts = Post.where('author_id', author.id).get()
+        else:
+            posts = Post.where('author_id', author.id).where_null('published_date').get()
+            
+        return view.render('blog/user_posts', {'posts': posts, 'author': author})
+
