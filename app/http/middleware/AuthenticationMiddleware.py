20f074e008a7690848ff56a9601cd06bee84b691
Vaibhav Mule <vaibhavmule135@gmail.com>
2018-10-10 12:27:57 +0530

    change in auth middleware

--- app/http/middleware/AuthenticationMiddleware.py
+++ app/http/middleware/AuthenticationMiddleware.py
@@ -21,7 +21,8 @@ class AuthenticationMiddleware:
         """
 
         if not self.request.user():
-            self.request.redirect_to('login')
+            self.request.redirect(f'/login?back={self.request.path}')
+
 
     def after(self):
         """Run This Middleware After The Route Executes
